44fcb877ad31e6881dddf6ac1db05ac06bbbce46
setiawanDedi05 <dssetiawan05@gmail.com>
2024-11-23 10:38:29 +0700

    first-landing-page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,55 @@
-import { Text, View } from "react-native";
-import React from "react";
+import CustomButton from "@/components/ui/CustomButton";
+import { images } from "@/constants";
+import { router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Link } from "expo-router";
+import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const index = () => {
+export default function index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-black">Hi, Dedi!</Text>
-      <Link href="/home" style={{ color: "blue" }}>
-        Home
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="w-[380px] h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Et sint ab nesciunt aperiam et atque{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[10px] absolute -bottom-1 right-12"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-regular text-gray-100 mt-7 text-center">
+            A dolor sint et consequatur ex aliquam ut a Cumque et non aliquam ex
+            Aora
+          </Text>
+          <CustomButton
+            title="Continue With Email"
+            handlePress={() => {
+              router.push("/sign-in")
+            }}
+            containerStyle="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
-};
-
-export default index;
+}
